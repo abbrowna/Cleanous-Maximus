d67c65bbeb82c4111dde051fc5c814529c6dbc8a
Anthony Richard Brown <abbrowna@gmail.com>
2017-11-03 12:12:16 +0300

    update

--- Bincode/Bincode/__vm/.Bincode.vsarduino.h
+++ Bincode/Bincode/__vm/.Bincode.vsarduino.h
@@ -1,16 +1,17 @@
 /* 
 	Editor: http://www.visualmicro.com
-	        visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
-	        the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
-	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
-	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+			visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+			the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
+			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
 	Hardware: Arduino/Genuino 101, Platform=arc32, Package=Intel
 */
 
+#if defined(_VMICRO_INTELLISENSE)
+
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define _VMDEBUG 1
 #define __ARDUINO_ARC__
 #define F_CPU 32000000L
 #define ARDUINO 10801
@@ -88,11 +89,12 @@ extern "C" void __cxa_pure_virtual() {;}
 
 
 
-#include <arduino.h>
+#include <Arduino.h>
 #include <pins_arduino.h> 
 #include <variant.h> 
 #include <variant.cpp> 
 #undef cli
 #define cli()
 #include "Bincode.ino"
+#endif
 #endif
